a17c598b38f58b810778bbdaf1cb9a231471ac1c
Isa Conteh <iconteh21@gmail.com>
2020-09-25 19:29:26 -0400

    route new home page and created post function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(8090, function(){
     console.log("app working on port 8090");
 });
 
 app.get('/', (req, res) => {
-    res.send('App cd\'s');
+    // res.send('App cd\'s');
+    res.render('template.ejs');
+});
+
+app.post('/show', (req, res) => {
+    console.log('Test');
 });
