41238565a0c03dd40491e7a2ea9f6f251c8f780b
Isa Conteh <iconteh21@gmail.com>
2020-09-25 19:39:44 -0400

    added mongo db client

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.listen(8090, function(){
@@ -14,4 +16,5 @@ app.get('/', (req, res) => {
 
 app.post('/show', (req, res) => {
     console.log('Test');
+    console.log(req.body);
 });
