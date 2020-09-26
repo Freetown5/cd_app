f54a922857f5974e7fa14eb307a1674a4dee751b
Isa Conteh <iconteh21@gmail.com>
2020-09-25 20:55:54 -0400

    connected mongo and hid api keys in env files

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {  useNewUrlParser: true });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    client.close();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
