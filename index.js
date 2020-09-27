40d6926fcbfc93820df8386acc4f043abcff1b1d
Isa Conteh <iconteh21@gmail.com>
2020-09-27 19:07:07 -0400

    fixed post request and created new show page

--- index.js
+++ index.js
@@ -5,26 +5,54 @@ const bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {  useNewUrlParser: true });
-
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    client.close();
-});
+const client = new MongoClient(uri, {  useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 
-app.listen(8090, function(){
-    console.log("app working on port 8090");
+client.connect(err => {
+    const collection = client.db("cd_app").collection("cds");
+
+    app.listen(8090, function(){
+        console.log("app working on port 8090");
+    });
+
+    app.get('/', (req, res) => {
+        const cursor = collection.find();
+    })
+
+    app.post('/show', (req, res) => {
+        collection.insertOne(req.body, (err, result) => {
+            if(err) return console.log("Error:" + err);
+    
+            console.log("Recorded successfully, saved to BD!");
+            res.redirect('/show');
+            collection.find().toArray((err, results) => {
+                console.log(results);
+            });
+        });
+    });
+
+    app.get('/show', (req, res) => {
+        collection.find().toArray((err, results) => {
+            if(err) return console.log("Error: " + err);
+            res.render('show.ejs', {cds: results});
+        });
+    });
+    // client.close();
 });
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     // res.send('App cd\'s');
     res.render('template.ejs');
 });
 
-app.post('/show', (req, res) => {
-    console.log('Test');
-    console.log(req.body);
-});
+// app.post('/show', (req, res) => {
+//     collection.insertOne(req.body, (err, result) => {
+//         if(err) return console.log("Error:" + err);
+
+//         console.log("Recorded successfully, saved to BD!");
+//         res.redirect('/');
+//     });
+// });
