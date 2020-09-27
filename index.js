078061ba72cf5f60b5bbc400af7cbc3390609890
Isa Conteh <iconteh21@gmail.com>
2020-09-27 19:38:49 -0400

    added update function and template

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const uri = `mongodb+srv://${process.env.API_USERNAME}:${process.env.API_KEY}@cluster0.lfwrb.mongodb.net/${process.env.API_APPNAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {  useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -36,6 +37,32 @@ client.connect(err => {
         collection.find().toArray((err, results) => {
             if(err) return console.log("Error: " + err);
             res.render('show.ejs', {cds: results});
+        });
+    });
+
+    app.route('/edit/:id')
+    .get((req, res) => {
+        const id = req.params.id;
+        collection.find(ObjectId(id)).toArray((err, result) => {
+            if(err) return console.log("Error: " + err);
+            res.render('edit.ejs', {cds: result});
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        const title = req.body.title;
+        const artist = req.body.artist;
+        const genre = req.body.genre;
+        collection.updateOne({_id: ObjectId(id)}, {
+            $set: {
+                title: title,
+                artist: artist,
+                genre: genre
+            }
+        }, (err, result) => {
+            if(err) return res.send(err);
+            res.redirect('/show');
+            console.log("Successfully Updated!");
         });
     });
     // client.close();
